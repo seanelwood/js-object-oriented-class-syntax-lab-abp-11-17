6dccc6bdc2c1ae5fffa749d748609b44dd9b80fd
Sean Elwood <seanelwood@gmail.com>
2017-11-14 14:26:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,48 @@
+class BoardMember {
+  constructor (name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;    
+  }
+  veto() {
+    return 'No, I must disagree';
+  }
+  approve() {
+    return 'You can do that!';
+  }
+  doCharity() {
+    return 'I like to help people.';
+  }
+  releasePressStatement() {
+    return 'You will see great things from Scuber.';
+  }
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`;  
+  }
+}
+
+
+
+
+
+
+// class User {
+//   constructor(name, email) {
+//     this.name = name;
+//     this.email = email;
+//   }
+//  
+//   sayHello() {
+//     console.log(`Hello, my name is ${this.name}`);
+//   }
+// }
+//  
+//  
+// class Teacher extends User {
+//   sayHello(){
+//     super.sayHello()
+//     console.log('hello')
+//   }
+// }
+//  
+// tom.sayHello()
