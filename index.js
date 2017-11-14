46226c5838e8423e5b4b1b74cf26ea6bea9b0600
Sean Elwood <seanelwood@gmail.com>
2017-11-14 14:27:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ class BoardMember {
     return 'You will see great things from Scuber.';
   }
   sayHi() {
-    return `Hi, my name is ${name}. I am from ${homestate}, and I was trained in ${training}.`;
+    return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`;
   }
 }
 
